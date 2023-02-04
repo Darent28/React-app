0d01d06e62507abcc26ade14423ad4e251245367
Darent28 <darentramdom@gmail.com>
2023-02-04 01:45:19 -0600

    Cambio

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './login.css';
 
 export const LogIn = () => {
     
     return(
         <body>
+           
+            <form align="center" class= "login">
             <h1 align="center">LogIn</h1>
             <p align="center">This Is LogIn site</p>
+                <div class="form-group">
+                    <label for="username">
+                        Username:
+                        <input type="text" class="form-control" name="name" aria-describedby="username" />
+                    </label>
+                </div>
+                <div class="form-group">
+                    <label for="password">
+                        Password:
+                        <input type="password" class="form-control" name="password" aria-describedby="username" />
+                    </label>
+                </div>
+                <br />
+                <button type="submit" class="btn btn-secondary">Submit</button>
+            </form>
+
         </body> 
     )
 }
